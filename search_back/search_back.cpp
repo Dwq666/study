d2e94236dcbb845f4a99125c418e1eaf75e0a552
Dwq <840482028@qq.com>
2022-09-07 00:46:58 +0800

    增加搜索与回溯算法

---
+++ search_back/search_back.cpp
@@ -0,0 +1,33 @@
+#include "search_back.h"
+
+/**
+ * @brief 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ *
+ * @param root 二叉树数据结构
+ * @return vector<int>
+ */
+vector<int> CSearchBack::levelOrder(TreeNode *root)
+{
+    vector<int> iRes;       //定义打印数组
+    queue<TreeNode *> mQue; //插入二叉树值的队列
+
+    if (!root)
+        return iRes;
+
+    mQue.push(root);
+
+    while (!mQue.empty())
+    {
+        TreeNode *node = mQue.front();
+        mQue.pop();
+
+        iRes.push_back(node->val);
+
+        if (node->left != nullptr)
+            mQue.push(node->left);
+        if (node->right != nullptr)
+            mQue.push(node->right);
+    }
+
+    return iRes;
+}
---
+++ search_back/search_back.h
@@ -0,0 +1,28 @@
+#ifndef SEARCH_BACK_H
+#define SEARCH_BACK_H
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class CSearchBack
+{
+public:
+    //从上到下打印二叉树
+    vector<int> levelOrder(TreeNode *root);
+    
+};
+
+#endif
